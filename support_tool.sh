9c73e985d412a9c42e99d2dd9e7f82d947d24073
philbur87 <schilumpf@aol.com>
2026-08-22 12:56:24 +0200

    Einfaches Menue hinzugefuegt

--- support_tool.sh
+++ support_tool.sh
@@ -8,3 +8,11 @@ echo "Bitte geben Sie Ihren Namen ein:"
 read name
 
 echo "Hallo $name, willkommen im IT Support Tool."
+
+echo ""
+
+echo "Bitte wählen Sie eine Aufgabe:"
+echo "1) Systeminformationen anzeigen"
+echo "2) Speicherplatz prüfen"
+echo "3) Programm beenden"
+read auswahl
